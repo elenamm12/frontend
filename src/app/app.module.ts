8f49a06d69f7c0dec1debf5e39ae77fb8b46f8ed
Maria G. Cafarelli <cafarelli172006@gmail.com>
2020-05-19 14:06:23 -0400

    Organizacion inicial de las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routing/routing.module';
 import { AppComponent } from './app.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';

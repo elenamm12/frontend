55d821bf6fd362e731f437a9cd243f150084ff81
Maria G. Cafarelli <cafarelli172006@gmail.com>
2020-05-20 22:06:08 -0400

    cosas

--- src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar-usuario.component.scss']
 })
 export class RegistrarUsuarioComponent implements OnInit {
-
+  imageUrl : string = "../../../assets/icon/usuario.png";
+  fileToUpload: File = null;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleFileInput(file: FileList){
+    this.fileToUpload = file.item(0);
+
+    var reader = new FileReader();
+    reader.onload = (event:any) => {
+      this.imageUrl = event.target.result;
+    }
+    reader.readAsDataURL(this.fileToUpload);
   }
 
 }

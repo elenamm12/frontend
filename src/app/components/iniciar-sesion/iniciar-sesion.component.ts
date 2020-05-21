e1b53f7bf1e61134c99b0adaca773505555ed732
Rafael Urdaneta <rafael.urdaneta@correo.unimet.edu.ve>
2020-05-21 16:55:06 -0430

    validacion registro

--- src/app/components/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/components/iniciar-sesion/iniciar-sesion.component.ts
@@ -9,7 +9,7 @@ import {FormControl, FormGroup, Validators} from '@angular/forms'
 export class IniciarSesionComponent implements OnInit {
   
   //expresion regular para validar email
-  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+private   emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   //
 
   createFormGroup (){
--- src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -1,17 +1,147 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators, FormBuilder} from '@angular/forms'
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
 
 @Component({
   selector: 'app-registrar-usuario',
   templateUrl: './registrar-usuario.component.html',
   styleUrls: ['./registrar-usuario.component.scss']
 })
-export class RegistrarUsuarioComponent implements OnInit {
+export class RegistrarUsuarioComponent implements OnInit{
   imageUrl : string = "../../../assets/icon/usuario.png";
   fileToUpload: File = null;
-  constructor() { }
+
+  private   emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+  createFormGroup (){
+    return new FormGroup({
+    nombres: new FormControl('', [
+      Validators.required,
+      Validators.pattern('')  
+    ]),
+    apellidos: new FormControl('', [
+      Validators.required,
+      Validators.pattern('')  
+    ]),
+    fecha: new FormControl('', [
+      Validators.required,
+      Validators.pattern('')  
+    ]),
+    correo: new FormControl('', [
+      Validators.required,
+      Validators.pattern(this.emailPattern)  
+    ]),  
+    usuario: new FormControl('', [
+      Validators.required,
+      Validators.pattern('')  
+    ]),
+    contra: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7)  
+    ]),
+    validContra: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7),
+      this.checkPasswords
+    ])
+    })
+  }
+
+  registerForm: FormGroup;
+
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      nombres: new FormControl('', [
+        Validators.required 
+      ]),
+      apellidos: new FormControl('', [
+        Validators.required
+      ]),
+      fecha: new FormControl('', [
+        Validators.required
+      ]),
+      correo: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.emailPattern)  
+      ]),  
+      usuario: new FormControl('', [
+        Validators.required,
+        Validators.pattern('')  
+      ]),
+      image:new FormControl(''),
+      contra: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7)  
+      ]),
+      validContra: new FormControl(''),
+      
+    }, { validator: this.checkPasswords });
+
+   }
 
   ngOnInit(): void {
   }
+
+  onResetForm() {
+    this.registerForm.reset();
+  }
+
+  onSaveForm(){
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value);
+      this.onResetForm();
+      }else{
+        console.log('No Valido')
+      }
+  }
+
+  checkPasswords(group: FormGroup) { 
+  let pass = group.controls.contra.value;
+  let confirmPass = group.controls.validContra.value;
+
+  return pass === confirmPass ? null : { notSame: true }     
+  }
+
+  get nombres(){
+    return this.registerForm.get('nombres')
+  };
+
+  get apellidos(){
+    return this.registerForm.get('apellidos')
+  };
+
+  get fecha(){
+    return this.registerForm.get('fecha')
+  };
+
+  get usuario(){
+    return this.registerForm.get('usuario')
+  };
+
+  get correo(){
+    return this.registerForm.get('correo')
+  };
+
+  get contra(){
+    return this.registerForm.get('contra')
+  };
+
+  get validContra(){
+    return this.registerForm.get('validContra')
+  };
+
 
   handleFileInput(file: FileList){
     this.fileToUpload = file.item(0);

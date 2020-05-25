7cea4e09429ebd8861abfdd4fd888c4e7456d0c6
Maria G. Cafarelli <cafarelli172006@gmail.com>
2020-05-25 10:27:12 -0400

    detalles ruteo

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IniciarSesionComponent } from '../components/iniciar-sesion/iniciar-sesion.component';
 import { NotFoundComponent } from '../components/not-found/not-found.component';
+import { MenuComponent } from '../components/menu/menu.component';
 
 
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
   { path: 'iniciar-sesion', component: IniciarSesionComponent },
   { path: 'registrar-usuario', component: RegistrarUsuarioComponent },
+  { path: 'home', component: MenuComponent },
   { path: "", redirectTo: "/inicio", pathMatch: "full" },
   { path: "**", component: NotFoundComponent }
 ];

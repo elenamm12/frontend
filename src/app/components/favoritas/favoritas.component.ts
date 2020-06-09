b977f404e99df886b0eb3d587663eff6846a9157
Maria G. Cafarelli <cafarelli172006@gmail.com>
2020-06-09 14:28:38 -0400

    jbk

--- src/app/components/favoritas/favoritas.component.ts
+++ src/app/components/favoritas/favoritas.component.ts
@@ -4,21 +4,18 @@ import { WaveServiceService } from 'src/app/services/wave-service.service';
 @Component({
   selector: 'app-favoritas',
   templateUrl: './favoritas.component.html',
-  styleUrls: ['./favoritas.component.scss']
+  styleUrls: ['./favoritas.component.scss'],
 })
 export class FavoritasComponent implements OnInit {
   //tiene las subcategorias favoritas;
-  favoriteCategories : any[] = [];
+  favoriteCategories: any[] = [];
 
-
-  constructor(private waveService : WaveServiceService) { }
+  constructor(private waveService: WaveServiceService) {}
 
   ngOnInit(): void {
-    this.waveService.getFavoriteSubCategories().subscribe((response)=>{
-     
+    this.waveService.getFavoriteSubCategories().subscribe((response) => {
       this.favoriteCategories = response.categories;
-      console.log("favorite", this.favoriteCategories);
+      console.log('favorite', this.favoriteCategories);
     });
   }
-
 }

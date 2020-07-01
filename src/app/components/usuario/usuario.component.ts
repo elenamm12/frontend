9c3b8c178d29cb0cb406f8035dd7406cb80996a7
Maria G. Cafarelli <cafarelli172006@gmail.com>
2020-06-30 20:07:56 -0400

    user

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -5,38 +5,37 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.scss']
+  styleUrls: ['./usuario.component.scss'],
 })
 export class UsuarioComponent implements OnInit {
   user: any;
-  forumsPosts:[];
-  notSubscribedForumsPosts:[];
-  forumsCreated:[];
+  forumsPosts: [];
+  notSubscribedForumsPosts: [];
+  forumsCreated: [];
 
   constructor(
     private waveService: WaveServiceService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.user = JSON.parse(this.waveService.getCurrentUser());
     //console.log(this.user);
-    this.waveService.getForumsPostsByUser().subscribe((res)=>{
+    this.waveService.getForumsPostsByUser().subscribe((res) => {
       this.forumsPosts = res.forums;
+      console.log(this.forumsPosts)
     });
 
-    this.waveService.getNotSubscribedByUser().subscribe((res)=>{
+    this.waveService.getNotSubscribedByUser().subscribe((res) => {
       this.notSubscribedForumsPosts = res.forums;
     });
-    this.waveService.getForumCreated().subscribe((res)=>{
+    this.waveService.getForumCreated().subscribe((res) => {
       this.forumsCreated = res.forums;
     });
-
   }
 
-onDelete(id:number){
-  this.waveService.DeletePost(id);
-}
-
+  onDelete(id: number) {
+    this.waveService.DeletePost(id);
+  }
 }

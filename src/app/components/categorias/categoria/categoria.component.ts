20af47e1b2dbf98dc198aba719ef57a39196af2a
Maria G. Cafarelli <cafarelli172006@gmail.com>
2020-07-03 20:22:24 -0400

    no olvidar categoria.component.ts amen

---
+++ src/app/components/categorias/categoria/categoria.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { WaveServiceService } from 'src/app/services/wave-service.service';
+
+@Component({
+  selector: 'app-categoria',
+  templateUrl: './categoria.component.html',
+  styleUrls: ['./categoria.component.scss'],
+})
+export class CategoriaComponent implements OnInit {
+  categories: any[] = [];
+  categoryById: any = [];
+  content0: any;
+  categoria: any;
+  subcategories: any[] = [];
+  categoryId: number;
+  contentCategory = [];
+
+  constructor(
+    private waveService: WaveServiceService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.categoryId = this.route.snapshot.params['id'];
+    this.waveService.getCategoryById(this.categoryId).subscribe((response) => {
+      console.log(response);
+      this.categoryById = response;
+      if (this.categoryById.contentCategories.length > 0) {
+        this.content0 = this.categoryById.contentCategories[0];
+      }
+      console.log('categoria', this.content0);
+
+      this.waveService.getContentCategory(this.categoryId).subscribe((response) => {
+        console.log(response);
+        this.contentCategory = response.Contents;
+
+        this.waveService
+          .getSubcategoryByCategory(this.categoryId)
+          .subscribe((response) => {
+            console.log(response);
+            this.subcategories = response.subCategories;
+            console.log(this.subcategories);
+          });
+      });
+    });
+  }
+}

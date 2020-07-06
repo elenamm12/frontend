f42c93d2d0b051702d2c90849a260dd28751a95c
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2020-07-06 14:47:56 -0400

    arreglo imagen contenido categoria

--- src/app/services/wave-service.service.ts
+++ src/app/services/wave-service.service.ts
@@ -5,7 +5,7 @@ import {
   HttpHeaders,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { Router, NavigationEnd } from '@angular/router'
+import { Router, NavigationEnd } from '@angular/router';
 import { Observable, BehaviorSubject, throwError } from 'rxjs';
 import { map, tap, catchError, retry } from 'rxjs/operators';
 import { isNullOrUndefined } from 'util';
@@ -24,7 +24,7 @@ export class WaveServiceService {
   private previousUrl: string;
   private currentUrl: string;
   private authSubject = new BehaviorSubject(false);
-  
+
   //
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
@@ -51,7 +51,7 @@ export class WaveServiceService {
       }
     });
   }
-  
+
   public getPreviousUrl() {
     return this.previousUrl;
   }
@@ -309,34 +309,48 @@ export class WaveServiceService {
     return this.http.get(`${this.url}/content-category/category/${id}`);
   }
 
-  CreateContent(id: number, title: string, text: string, link: string): Observable<any>  {
-    return this.http.post(`${this.url}/content-category/create/category/${id}`, { text, title, link });
+  CreateContent(
+    id: number,
+    title: string,
+    text: string,
+    link: string
+  ): Observable<any> {
+    return this.http.post(
+      `${this.url}/content-category/create/category/${id}`,
+      { text, title, link }
+    );
   }
 
-  SavePicContent(id:number, files: File[]){
+  SavePicContent(id: number, files: File[]) {
     console.log(files[0]);
     let file = files[0];
     const fd = new FormData();
     fd.append('file', file, file.name);
-    return this.http.post(`${this.url}/content-category/photo/upload/${id}`, {fd});
+    return this.http.post(
+      `${this.url}/content-category/photo/upload/${id}`,
+      fd
+    );
   }
 
-  disableContent(id: number): Observable<any>{
-    return this.http.patch(`${this.url}/content-category/disable/${id}`, []);  
-
+  disableContent(id: number): Observable<any> {
+    return this.http.patch(`${this.url}/content-category/disable/${id}`, []);
   }
 
-  enableContent(id: number): Observable<any>{
-    return this.http.patch(`${this.url}/content-category/activate/${id}`, []);  
-
+  enableContent(id: number): Observable<any> {
+    return this.http.patch(`${this.url}/content-category/activate/${id}`, []);
   }
 
-  updateContent(id: number,  title: string, text: string, link: string): Observable<any>  {
+  updateContent(
+    id: number,
+    title: string,
+    text: string,
+    link: string
+  ): Observable<any> {
     console.log(id, title, text, link);
-    return this.http.post(`${this.url}/content-category/update/${id}`, { text, title, link });
-  } 
-
-
-
-
+    return this.http.post(`${this.url}/content-category/update/${id}`, {
+      text,
+      title,
+      link,
+    });
+  }
 }
